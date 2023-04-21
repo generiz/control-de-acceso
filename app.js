2ee5c6e8ef82adb55a394c8900001c58f8698998
generiz <39973192+generiz@users.noreply.github.com>
2023-04-21 13:28:51 -0600

    Configuración inicial del proyecto

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const speakeasy = require('speakeasy');
+const QRCode = require('qrcode');
+
+const app = express();
+app.use(express.json());
+
+app.get('/generate', (req, res) => {
+  const secret = speakeasy.generateSecret({ length: 20 });
+  QRCode.toDataURL(secret.otpauth_url, (err, data_url) => {
+    res.json({
+      secret: secret.base32,
+      dataURL: data_url,
+      otpURL: secret.otpauth_url,
+    });
+  });
+});
+
+app.post('/validate', (req, res) => {
+  const { token, secret } = req.body;
+  const verified = speakeasy.totp.verify({
+    secret: secret,
+    encoding: 'base32',
+    token: token,
+  });
+
+  res.json({ verified: verified });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
